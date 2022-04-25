8e7028638bac73bdd90c04eb666a0185bead84b1
Ewelka <ewelina.boczar@student.pk.edu.pl>
2022-04-25 21:28:48 +0200

    starting with React.js /first_page

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,60 @@
+class App extends React.Component {
+    render(){
+        return(
+            <>
+                <div class="upper">
+                    <p>Welcome to </p>
+                    <p id='tittle'>Cookmy.</p>
+                </div>
+                <StartInfo number="1"/>
+                <div class="dots">
+                    <DotsNav isClicked="true" number="1"/>
+                    <DotsNav isClicked="false" number="2"/>
+                    <DotsNav isClicked="false" number="3"/>
+                    <DotsNav isClicked="false" number="4"/>
+                </div>
+            </>
+        )
+    }
+}
 
+const StartInfo = (props) => {
+    const num = parseInt(props.number)
+    console.log("startinfo")
+    if(num === 1){
+        return(
+                <div class="info-panel">
+                    <img src="../img/deska.png"></img>
+                    <p>Start collecting new recipes in one place.</p>
+                </div>
+        )
+    }
+}
+
+class DotsNav extends React.Component {
+    state = {
+        isClicked: this.props.isClicked
+    }
+
+    StartInfo = (props) => {
+        const num = parseInt(props.number)
+        console.log("startinfo")
+        if(num === 1){
+            return(
+                    <div class="info-panel">
+                        <img src="../img/deska.png"></img>
+                        <p>Start collecting new recipes in one place.</p>
+                    </div>
+            )
+        }
+    }
+
+    render(){
+        return(
+            <button id="dot" onClick={() => this.StartInfo(2)}></button>
+        )
+    }
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
